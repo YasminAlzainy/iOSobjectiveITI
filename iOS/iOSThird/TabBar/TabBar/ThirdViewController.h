0ee5087695daaf825ab1abdb72e9802438210d26
Yasmin Ayman Al-Zainy <yasmin.alzainy@gmail.com>
2019-04-22 17:45:42 +0200

    22/4

---
+++ iOS/iOSThird/TabBar/TabBar/ThirdViewController.h
@@ -0,0 +1,17 @@
+//
+//  ThirdViewController.h
+//  TabBar
+//
+//  Created by JETS Mobile Lab - 8 on 4/16/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThirdViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS/iOSThird/TabBar/TabBar/ThirdViewController.m
@@ -0,0 +1,32 @@
+//
+//  ThirdViewController.m
+//  TabBar
+//
+//  Created by JETS Mobile Lab - 8 on 4/16/19.
+//  Copyright © 2019 ITI. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
